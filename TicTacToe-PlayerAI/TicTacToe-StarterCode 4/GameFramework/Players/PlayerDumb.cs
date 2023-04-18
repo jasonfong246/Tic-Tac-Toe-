cbd163382cfb9e16d3dd904e87e986e0d33e4055
jasonfong246 <122415404+jasonfong246@users.noreply.github.com>
2023-04-18 12:14:37 -0400

    Add files via upload

---
+++ TicTacToe-PlayerAI/TicTacToe-StarterCode 4/GameFramework/Players/PlayerDumb.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+
+namespace CrawfisSoftware.TicTacToeFramework
+{
+    public class PlayerDumb : IPlayer
+    {
+        private readonly IGameBoard<int, CellState> gameBoard;
+        private readonly IQueryGameState<int, CellState> gameState;
+        private readonly System.Random random;
+        private readonly CellState cellMarker;
+
+        public PlayerDumb(CellState cellMarker, IGameBoard<int, CellState> gameBoard, IQueryGameState<int, CellState> gameState)
+            : this(cellMarker, gameBoard, gameState, new System.Random())
+        {
+        }
+
+        public PlayerDumb(CellState cellMarker, IGameBoard<int, CellState> gameBoard, IQueryGameState<int, CellState> gameState, System.Random random)
+        {
+            this.cellMarker = cellMarker;
+            this.gameBoard = gameBoard;
+            this.gameState = gameState;
+            this.random = random;
+        }
+
+        public void TakeTurn()
+        {
+            Console.WriteLine("Player O's turn.");
+            var availableCells = gameState.GetMatchingCells(CellState.Blank).ToList();
+            if (availableCells.Count > 0)
+            {
+                int index = random.Next(availableCells.Count);
+                gameBoard.ChangeCellAttempt(availableCells[index], cellMarker);
+               
+            }
+        }
+    }
+}
